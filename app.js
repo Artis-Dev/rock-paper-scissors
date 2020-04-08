adfe059e601b51fea0d28f002a4f9f9389e66d8d
Artis-Dev <admin@moonart.lt>
2020-04-08 19:51:31 +0300

    Add pc and npc player functions

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log('?');
+function npcPlay() {
+  const moves = ['rock', 'paper', 'scissors'];
+
+  function randomMove() {
+    return moves[Math.floor(Math.random() * moves.length)];
+  }
+
+  return randomMove();
+}
+
+function pcPlay() {
+  let pcMove = prompt('Your move (Rock, Paper or Scissors):');
+  return pcMove.toLowerCase();
+}
